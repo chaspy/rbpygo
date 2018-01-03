05dfed54a00fa9b64dd411927eef0c2f8895a5a9
kondo takeshi <take.she12@gmail.com>
2018-01-03 15:37:25 +0900

    add fizzbuzz tests

--- 2_fizzbuzz/fizzbuzz.rb
+++ 2_fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,9 @@
-for i in 1..100
-  if i % 15 == 0
-    puts "fizzbuzz"
-  elsif i % 3 == 0
-    puts "fizz"
-  elsif i % 5 == 0
-    puts "buzz"
-  else
-    puts i
+class FizzBuzz
+  def fizzbuzz(num)
+  return "FizzBuzz" if num % 15 == 0
+  return "Fizz" if num % 3 == 0
+  return "Buzz" if num % 5 == 0
+  return num
   end
 end
+
